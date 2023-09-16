0cfd22118be44a7eacb67fd7b4cf61606455f620
falyArivelo <arivelofaly@gmail.com>
2023-09-16 19:03:10 +0300

    NOT

--- process.php
+++ process.php
@@ -1,49 +1,64 @@
-<?php 
+<?php
 
-    $x = 0;
-    $y=1;
-    // Table boolean
-    
-
-    //ET
-    echo "--AND--";
-
-    if($x == 0 && $y ==0){
-        echo 0;
-    }
-
-    if($x == 0 && $y ==1){
-        echo 0;
-    }
-
-    if($x == 1 && $y ==0){
-        echo 0;
-    }
-
-    if($x == 1 && $y ==1){
-        echo 1;
-    }
-
-    echo "<br>";
-
-    //OU
-    echo "--OR--";
+$x = 0;
+$y = 1;
+// Table boolean
 
 
-    if($x == 0 && $y ==0){
-        echo 0;
-    }
+//ET
+echo "--AND--";
 
-    if($x == 0 && $y ==1){
-        echo 1;
-    }
+if ($x == 0 && $y == 0) {
+    echo 0;
+}
 
-    if($x == 1 && $y ==0){
-        echo 1;
-    }
+if ($x == 0 && $y == 1) {
+    echo 0;
+}
 
-    if($x == 1 && $y ==1){
-        echo 1;
-    }
+if ($x == 1 && $y == 0) {
+    echo 0;
+}
 
-    echo "<br>";
+if ($x == 1 && $y == 1) {
+    echo 1;
+}
+
+echo "<br>";
+
+//OU
+echo "--OR--";
+
+
+if ($x == 0 && $y == 0) {
+    echo 0;
+}
+
+if ($x == 0 && $y == 1) {
+    echo 1;
+}
+
+if ($x == 1 && $y == 0) {
+    echo 1;
+}
+
+if ($x == 1 && $y == 1) {
+    echo 1;
+}
+
+echo "<br>";
+
+//NOT
+echo "--NOT--";
+
+
+if ($x == 0) {
+    echo 1;
+}
+
+if ($x == 1) {
+    echo 0;
+}
+
+
+echo "<br>";
